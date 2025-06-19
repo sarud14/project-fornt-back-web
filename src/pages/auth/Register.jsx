db8a1b260d661d4808574326a9f7ec34175d78cf
sarud14 <ruzddchan@yahoo.com>
2025-06-19 14:46:16 +0700

    add loading in button

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,19 +1,23 @@
 import FormInput from "../../components/form/FormInput";
 import { createAlert } from "../../utils/createAlert";
 import { useForm } from "react-hook-form";
-import axios from "axios"
+import axios from "axios";
 
 export default function Register() {
-  const { register, handleSubmit } = useForm();
-
+  const { register, handleSubmit, formState } = useForm();
+  const { isSubmitting } = formState;
   const hdlSubmit = async (value) => {
-  
+    await new Promise ((resolve)=> setTimeout(resolve, 2000) )
     try {
-      const res = await axios.post("http://localhost:8000/auth/register", value)
-      console.log(res)
+      const res = await axios.post(
+        "http://localhost:8000/auth/register",
+        value
+      );
+      console.log(res);
+      createAlert("success", res.data.msg);
     } catch (error) {
-      console.log(error)
-       createAlert("info", error.response?.data?.msg);
+      console.log(error);
+      createAlert("info", error.response?.data?.msg);
     }
   };
 
@@ -26,7 +30,11 @@ export default function Register() {
           <FormInput register={register} name="name" />
           <FormInput register={register} name="password" />
           <FormInput register={register} name="confirmPassword" />
-          <button className="btn btn-neutral mt-4">Register</button>
+          <button className="btn btn-neutral mt-4">
+            {
+            isSubmitting ? "Loading" : "Register"
+            }
+            </button>
         </fieldset>
       </form>
     </div>
